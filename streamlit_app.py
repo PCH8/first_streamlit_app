df8d739eeeb1f90746f26f54bb35a39f34e87d92
PCH8 <pamela.in2000@gmail.com>
2022-11-22 21:20:21 -0500

    Update streamlit_app.py
    removed apostophe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Mom"'"s new healthy dinner')
+streamlit.title('My Moms new healthy dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 omega 3 and blueberry oatmeal')
 streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
