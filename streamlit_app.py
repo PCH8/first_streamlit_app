9f3f95f09d89cb139510a0eb6428a9a786e1775e
PCH8 <pamela.in2000@gmail.com>
2022-11-22 21:11:46 -0500

    Update streamlit_app.py
    added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('my parents new healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('omega 3 and blueberry oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('hardboiled free range egg')
